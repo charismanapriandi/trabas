609ebc714f1f7b8c8aeb7b0bc30ada175e2e3433
rismantrdr <rismantrdr@gmail.com>
2021-08-28 15:52:47 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
